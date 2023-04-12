6073a2a4356295027f529115faa4d5f46a1a8474
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-04-12 01:47:57 -0300

    ✬ Beach model

---
+++ src/models/beach.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+export enum BeachPosition {
+  N = 'N',
+  S = 'S',
+  E = 'E',
+  W = 'W',
+}
+
+export interface Beach {
+  _id?: string;
+  lat: number;
+  lng: number;
+  name: string;
+  position: BeachPosition;
+  user: string;
+}
+
+const schema = new mongoose.Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export const Beach = mongoose.model<Beach>('Beach', schema);
