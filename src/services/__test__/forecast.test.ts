b8d07ad5494c07bfa38f57663f8a577b558846c9
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-04-25 19:55:05 -0300

    ✬ Rating module - finished rating algorithm

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,7 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 import { Forecast, ForecastProcessingInternalError } from '../forecast';
-import { Beach, BeachPosition } from '@src/models/beach';
+import { Beach, GeoPosition } from '@src/models/beach';
 import apiForecastResponseFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 
 jest.mock('@src/clients/stormGlass');
@@ -22,7 +22,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
+        position: GeoPosition.E,
         user: 'fake-id',
       },
     ];
@@ -45,7 +45,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
+        position: GeoPosition.E,
         user: 'fake-id',
       },
     ];
